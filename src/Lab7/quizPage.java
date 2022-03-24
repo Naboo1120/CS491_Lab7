2a137e34636153350729ce0ef1bd085718bd7229
Ian <ianhathorn@gmail.com>
2022-03-23 22:52:05 -0500

    Fixed

--- src/Lab7/quizPage.java
+++ src/Lab7/quizPage.java
@@ -1,16 +1,24 @@
 package Lab7;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class quizPage extends JFrame{
     private JPanel quizPagePanel;
     private JLabel Hello;
 
+public quizPage() {
+    JFrame frame = new JFrame("Quiz Page");
+    frame.setContentPane(quizPagePanel);
+    frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    frame.pack();
+    frame.setVisible(true);
+}
+
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Lab7.Quiz Page");
-        frame.setContentPane(new quizPage().quizPagePanel);
-        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+
     }
+
+
 }
--- src/Lab7/startPage.java
+++ src/Lab7/startPage.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class startPage extends JFrame{
+public class startPage extends JFrame {
     private JLabel mathQuizLabel;
     private JButton startQuizButton;
     private JLabel description;
@@ -12,24 +12,31 @@ public class startPage extends JFrame{
     private JPanel field;
 
 
-    public startPage() {
-        startQuizButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-               quizPage quizPage = new quizPage();
-               quizPage.setContentPane(Lab7.quizPage);
-               quizPage.setVisible(true);
-            }
-        });
-    }
+public startPage(){
+
+    this.setContentPane(field);
+    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    this.pack();
+
+
+    startQuizButton.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            quizPage qP = new quizPage();
+            qP.show();
+
+            dispose();
+        }
+    });
+}
+
+
 
     public static void main(String args[]){
-            JFrame frame = new JFrame("Start Page");
-            frame.setContentPane(new startPage().field);
-            frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-            frame.pack();
-            frame.setVisible(true);
 
+        JFrame frame = new startPage();
+        frame.setVisible(true);
 
         }
+
 }
