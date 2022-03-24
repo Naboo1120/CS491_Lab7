b804261155218bff60355cab453c7c93ac61967a
Ian <ianhathorn@gmail.com>
2022-03-24 18:14:55 -0500

    Formatted the quizPage

--- src/Lab7/Quiz.java
+++ src/Lab7/Quiz.java
@@ -1,4 +1,11 @@
 package Lab7;
 
 public class Quiz {
+
+    public static String Question1;
+
+    class Question1{
+        String Question1 = new String("What is 5 x 5 ?");
+    }
+
 }
--- src/Lab7/quizPage.java
+++ src/Lab7/quizPage.java
@@ -6,15 +6,62 @@ import java.awt.event.ActionListener;
 
 public class quizPage extends JFrame{
     private JPanel quizPagePanel;
-    private JLabel Hello;
+    private JLabel Q1;
+    private JLabel Q2;
+    private JLabel Q3;
+    private JLabel Q4;
+    private JLabel Q5;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
 
-public quizPage() {
+    public quizPage() {
     JFrame frame = new JFrame("Quiz Page");
     frame.setContentPane(quizPagePanel);
     frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
     frame.pack();
     frame.setVisible(true);
-}
+
+
+
+
+        textField5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        textField4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        textField3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        textField2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        textField1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 
     public static void main(String[] args) {
 
